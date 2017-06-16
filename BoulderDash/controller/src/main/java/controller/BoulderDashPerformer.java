5552f13de80b441b946a18856b20b243a0bfdab2
Nazazz <charles.leleux@viacesi.fr>
2017-06-16 16:06:25 +0200

    BoulderDashPerformer IOrderPerfomer IUserOrder Order UserOrder
    Stub code

---
+++ BoulderDash/controller/src/main/java/controller/BoulderDashPerformer.java
@@ -0,0 +1,45 @@
+package controller;
+
+import model.IBoulderDashModel;
+
+public class BoulderDashPerformer implements IOrderPerformer{
+	
+	
+	
+	/**
+	 * create constructor
+	 * @param BoulderDashModel
+	 */
+	public BoulderDashPerformer(IBoulderDashModel BoulderDashModel) {}
+	
+
+	/**
+	 * create method play
+	 */
+	public void play() {}
+	
+	
+	/**
+	 * Sets the ViewSystem
+	 * 
+	 * @param viewSystem
+	 */
+	public void setViewSystem(IViewSystem viewSystem) {}
+	
+	/**
+	 * create a loop
+	 */
+	private void gameLoop() {}
+
+	
+	/**
+	 * implement OderPerformer
+	 * @param userOrder
+	 */
+	
+	@Override
+	public void OderPerform(UserOrder userOrder) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ BoulderDash/controller/src/main/java/controller/IUserOrder.java
@@ -0,0 +1,13 @@
+package controller;
+
+/**
+ * 
+ * @author charl
+ * @version 1.0
+ */
+
+public interface IUserOrder {
+
+	public Order getOrder();
+	
+}
