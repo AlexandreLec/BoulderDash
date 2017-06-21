7fe4943220141484cae92064a9ffa01a088c11d2
tapafe <arnaud.rigaut@viacesi.fr>
2017-06-21 13:18:01 +0200

    keyPressed

--- BoulderDash/view/src/main/java/view/IElementBuilder.java
+++ BoulderDash/view/src/main/java/view/IElementBuilder.java
@@ -1,10 +1,13 @@
 package view;
 
+import model.IBoulderDashModel;
+
 import java.awt.Graphics;
 import java.awt.image.ImageObserver;
 
 public interface IElementBuilder {
 
 	public void applyModelToGraphic(Graphics graphics, ImageObserver obs);
+	public IBoulderDashModel getModel();
 	
 }
