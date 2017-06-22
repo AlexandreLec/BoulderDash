da6466d1feeda5b4ac4e8814cd08261e3aaf9f07
Nazazz <charles.leleux@viacesi.fr>
2017-06-22 17:11:48 +0200

    correction

--- BoulderDash/view/src/main/java/view/IElementBuilder.java
+++ BoulderDash/view/src/main/java/view/IElementBuilder.java
@@ -12,16 +12,14 @@ import java.awt.image.ImageObserver;
 		 */
 public interface IElementBuilder {
 
-<<<<<<< HEAD
+
 		/**
 		 * Translate The Model into Graphic 
 		 * @param graphics
 		 * @param obs
 		 */
-	void applyModelToGraphic(Graphics graphics, ImageObserver obs);
-=======
 	void applyModelToGraphic(Graphics graphics, ImageObserver obs) throws Exception;
->>>>>>> branch 'master' of https://github.com/AlexandreLec/BoulderDash
+
 	IBoulderDashModel getModel();
 	
 }
